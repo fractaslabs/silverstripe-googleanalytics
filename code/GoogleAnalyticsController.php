23b22951224eb2fdd293a2faaa3a8391ab6fea94
helpfulrobot <chris@silverstripe.com>
2015-12-26 10:55:14 +1300

    Converted to PSR-2

--- code/GoogleAnalyticsController.php
+++ code/GoogleAnalyticsController.php
@@ -1,58 +1,65 @@
 <?php
 
-class GoogleAnalyticsController extends Extension {
+class GoogleAnalyticsController extends Extension
+{
 
-	private static $ga_id;
+    private static $ga_id;
 
-	private static $enable_display_features = true;
+    private static $enable_display_features = true;
 
-	private static $enable_in_dev = false;
+    private static $enable_in_dev = false;
 
-	public function GAID() {
-		if ($this->IsEnabled()) {
-			return Config::inst()->get('GoogleAnalyticsController', 'ga_id');
-		}
-	}
+    public function GAID()
+    {
+        if ($this->IsEnabled()) {
+            return Config::inst()->get('GoogleAnalyticsController', 'ga_id');
+        }
+    }
 
-	public function IsDisplayFeatured() {
-		return Config::inst()->get('GoogleAnalyticsController', 'enable_display_features');
-	}
+    public function IsDisplayFeatured()
+    {
+        return Config::inst()->get('GoogleAnalyticsController', 'enable_display_features');
+    }
 
-	public function IsEnabledInDev() {
-		return Config::inst()->get('GoogleAnalyticsController', 'enable_in_dev');
-	}
+    public function IsEnabledInDev()
+    {
+        return Config::inst()->get('GoogleAnalyticsController', 'enable_in_dev');
+    }
 
-	public function IsEnabled() {
-		if (Director::isLive()) {
-			return true;
-		} elseif (Director::isDev() && $this->IsEnabledInDev()) {
-			return true;
-		}
+    public function IsEnabled()
+    {
+        if (Director::isLive()) {
+            return true;
+        } elseif (Director::isDev() && $this->IsEnabledInDev()) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	public function MultiTrackersList() {
-		$list = Config::inst()->get('GoogleAnalyticsController', 'ga_extra_ids');
+    public function MultiTrackersList()
+    {
+        $list = Config::inst()->get('GoogleAnalyticsController', 'ga_extra_ids');
 
-		if (!is_array($list)) {
-			return false;
-		}
+        if (!is_array($list)) {
+            return false;
+        }
 
-		$extra = array();
-		foreach ($list as $key => $value) {
-			$extra[] = new ArrayData(array(
-				'Title' => $key,
-				'ID'    => $value
-			));
-		}
+        $extra = array();
+        foreach ($list as $key => $value) {
+            $extra[] = new ArrayData(array(
+                'Title' => $key,
+                'ID'    => $value
+            ));
+        }
 
-		return new ArrayList($extra);
-	}
+        return new ArrayList($extra);
+    }
 
-	public function onAfterInit() {
-		if ($this->GAID()) {
-			Requirements::insertHeadTags($this->owner->renderWith('GoogleAnalyticsCode'), 'GoogleAnalyticsCode');
-		}
-	}
+    public function onAfterInit()
+    {
+        if ($this->GAID()) {
+            Requirements::insertHeadTags($this->owner->renderWith('GoogleAnalyticsCode'), 'GoogleAnalyticsCode');
+        }
+    }
 }
